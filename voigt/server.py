c5c74063bdf3c55c9bc646c6c621aa5500a93566
Matthew <matthew.a.chatham@gmail.com>
2019-05-08 01:52:51 -0700

    fix server.py

--- voigt/server.py
+++ voigt/server.py
@@ -3,10 +3,21 @@ from sqlalchemy import create_engine
 import sqlite3
 import os
 
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css', 'https://gist.githubusercontent.com/mchatham-fk/852b69b35c20add71cb438a8d98b37cd/raw/3f1f3030762f0a29904538255ef5b49bd3d562f4/stylesheet.css']
+if os.environ.get('STACK'):
+    print('RUNNING ON HEROKU')
+    env = 'Heroku'
+    BASE_DIR = '/app'
+    DATABASE_URL = os.environ['DATABASE_URL']
+else:
+    env = 'Dev'
+    BASE_DIR = '/Users/matthew/freelance/voigt'
+
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css',
+                        'https://gist.githubusercontent.com/mchatham-fk/852b69b35c20add71cb438a8d98b37cd/raw/3f1f3030762f0a29904538255ef5b49bd3d562f4/stylesheet.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
-app.config['suppress_callback_exceptions']=True
+app.config['suppress_callback_exceptions'] = True
 
-dbconn = create_engine(DATABASE_URL) if os.environ.get('STACK') else sqlite3.connect('output.db')
+dbconn = create_engine(DATABASE_URL) if os.environ.get(
+    'STACK') else sqlite3.connect('output.db')
